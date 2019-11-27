ec8517f4cbd23ce577aa2557be30043887781438
PatricSPires <patric.pires@outlook.com>
2019-11-26 21:07:23 -0300

    feat(modulo1): create an array with users

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const server = express();
 
+const users = ['Diego', 'Claudio', 'Victor']
+
 server.get('/teste', (req, res) => {
   const nome = req.query.nome;
   return res.json({ message: `Hello ${nome}`})
 });
 
-server.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-  return res.json({ message: `Buscando usuário ${id}`})
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index])
 });
 
 server.listen(3000)
