a036b2004b66fbae56d657457e8af9809b4999fd
PatricSPires <patric.pires@outlook.com>
2019-11-30 00:26:54 -0300

    feat(modulo02): configure code style

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
+
 const routes = new Router();
 
-routes.get('/', (req, res)=>{
-  return res.json({message: "Hiiii"})
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hiiii' });
 });
 
 export default routes;
