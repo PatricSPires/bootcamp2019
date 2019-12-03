6acf5851a9f77ce5cff25761d3e62eb7fb00e25c
PatricSPires <patric.pires@outlook.com>
2019-12-03 20:38:41 -0300

    feat(model): create loader and insert user into db

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,16 @@
+import User from './app/models/User';
+
 const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hiiii' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Patric Pires',
+    email: 'patric.pires@outlook.com',
+    password_hash: 'oescritoriodetras',
+  });
+  return res.json(user);
 });
 
 export default routes;
