01b47e9092f26eabf74188f7c326e5519ed63ec5
PatricSPires <patric.pires@outlook.com>
2019-12-08 23:27:39 -0300

    feat(routes): remove unused route

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -7,13 +7,4 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Patric Pires',
-    email: 'patric.pires@outlook.com',
-    password_hash: 'oescritoriodetras',
-  });
-  return res.json(user);
-});
-
 export default routes;
