82b694153af5f64499353342fc7a7efc39c42d50
PatricSPires <patric.pires@outlook.com>
2019-12-16 10:41:31 -0300

    feat(appointment): list user appointments

--- modulo02/src/app/controllers/AppointmentController.js
+++ modulo02/src/app/controllers/AppointmentController.js
@@ -1,9 +1,18 @@
-import Appointment from '../models/Appointment';
+import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
-import * as Yup from 'yup';
+import Appointment from '../models/Appointment';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+    });
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -27,7 +36,7 @@ class AppointmentController {
         .json({ error: 'You only can create appointments with providers' });
     }
 
-    //Check for past dates
+    // Check for past dates
     const hourStart = startOfHour(parseISO(date));
     if (isBefore(hourStart, new Date())) {
       return res.status(400).json({ error: 'Past dates are not permitted' });
