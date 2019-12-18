7655b89a2ce7e11297f66aa63bebf490f5be60bc
PatricSPires <patric.pires@outlook.com>
2019-12-18 16:50:12 -0300

    refactor(AppointmentModel): remove assosicate

--- modulo02/src/app/models/Appointment.js
+++ modulo02/src/app/models/Appointment.js
@@ -6,17 +6,14 @@ class Appointment extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        user_id: Sequelize.INTEGER,
+        provider_id: Sequelize.INTEGER,
       },
       {
         sequelize,
       }
     );
     return this;
-  }
-
-  static associate(models) {
-    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
-    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
 
