f1db8de675bb2308b9aa8ba1a6bca4e8adf13a6f
PatricSPires <patric.pires@outlook.com>
2020-01-10 19:55:25 -0300

    feat(SessionController): create and call into routes file

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 
 // CONTROLLERS
 import UserController from './app/controllers/User';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 // USER CONTROLLER METHODS
 routes.post('/users', UserController.store);
+// SESSION CONTROLLER METHODS
+routes.post('/session', SessionController.store);
 
 module.exports = routes;
