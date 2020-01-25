e9945fdf38368cde8ff1d9d227ec211091ba83c6
PatricSPires <patric.pires@outlook.com>
2020-01-25 16:37:10 -0300

    feat(yup): add schema to validade data

--- gobarber/src/app/controllers/UserController.js
+++ gobarber/src/app/controllers/UserController.js
@@ -1,8 +1,25 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const userExists = await User.findOne({ where: { email: req.body.email } });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
@@ -19,6 +36,28 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, password) =>
+          oldPassword ? password.required() : password
+        ),
+      confirmPassword: Yup.string().when(
+        'password',
+        (password, confirmPassword) =>
+          password
+            ? confirmPassword.required().oneOf([Yup.ref('password')])
+            : confirmPassword
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
