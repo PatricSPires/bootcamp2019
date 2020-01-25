fa14b5f5294b29d3e903007deeee0a757e666488
PatricSPires <patric.pires@outlook.com>
2020-01-25 17:25:27 -0300

    feat(multer): configure multer

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 // CONTROLLERS
 import UserController from './app/controllers/UserController';
@@ -7,6 +9,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // USER CONTROLLER METHODS
 routes.post('/users', UserController.store);
@@ -15,5 +18,10 @@ routes.put('/users', UserController.update);
 
 // SESSION CONTROLLER METHODS
 routes.post('/session', SessionController.store);
+
+// UPLOAD METHODS
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 module.exports = routes;
