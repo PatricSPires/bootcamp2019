82a9d9a39cc88930d44b957f492df1651941df81
PatricSPires <patric.pires@outlook.com>
2020-02-18 16:27:53 -0300

    feat(main): loading api data from repository details

--- modulo05/src/pages/Repository/index.js
+++ modulo05/src/pages/Repository/index.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    isLoading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repositoryName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repositoryName}`),
+      api.get(`/repos/${repositoryName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      isLoading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, isLoading } = this.state;
+    return <h1>Chora</h1>;
+  }
 }
