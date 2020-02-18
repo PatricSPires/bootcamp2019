1e42e7d99eb51dc1878571283776401de5f76f00
PatricSPires <patric.pires@outlook.com>
2020-02-18 13:11:47 -0300

    feat(main): add main styles

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-import { Title } from './styled';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styled';
 
 export default function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositorios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositorio" />
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
